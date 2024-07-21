4cde22517420108ef277f74840cde1a7242dcb82
Georgiy Kamnev <nt.gocha@gmail.com>
2024-07-22 00:47:59 +0500

    ident

--- src/main/java/xyz/cofe/json/stream/token/IndentTokenWriter.java
+++ src/main/java/xyz/cofe/json/stream/token/IndentTokenWriter.java
@@ -1,6 +1,8 @@
 package xyz.cofe.json.stream.token;
 
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.function.Consumer;
 
 public class IndentTokenWriter implements TokenWriter {
     private final TokenWriter output;
@@ -9,196 +11,294 @@ public class IndentTokenWriter implements TokenWriter {
         this.output = output;
     }
 
-    private int level = 0;
-
-    public interface State {
-        int getItemsCount();
-        void incItemsCount();
+    private enum ItemType {
+        JsObj,
+        JsArr,
+        BigInt,
+        Long,
+        Int,
+        Double,
+        String,
+        Bool,
+        Null,
+        Ident
     }
-    public static class ArrState implements State {
+
+    private static abstract class BaseState {
         public int itemsCount = 0;
+        public int jsObjCount = 0;
+        public int jsArrCount = 0;
+        public int bigIntCount = 0;
+        public int longCount = 0;
+        public int intCount = 0;
+        public int doubleCount = 0;
+        public int stringCount = 0;
+        public int boolCount = 0;
+        public int nullCount = 0;
+        public int identCount = 0;
+        public int nestedWriteCount = 0;
 
-        @Override
-        public int getItemsCount() {
-            return itemsCount;
+        public int compoundCount() {
+            return jsArrCount + jsObjCount;
         }
 
-        public void incItemsCount(){
+        public void addItem(ItemType type) {
             itemsCount++;
+            switch (type) {
+                case Int -> intCount++;
+                case Bool -> boolCount++;
+                case Long -> longCount++;
+                case Null -> nullCount++;
+                case Ident -> identCount++;
+                case JsArr -> jsArrCount++;
+                case JsObj -> jsObjCount++;
+                case BigInt -> bigIntCount++;
+                case Double -> doubleCount++;
+                case String -> stringCount++;
+            }
         }
     }
-    public static class ObjState implements State {
-        public int itemsCount = 0;
-
-        @Override
-        public int getItemsCount() {
-            return itemsCount;
-        }
-
-        public void incItemsCount(){
-            itemsCount++;
-        }
+    private static class ArrState extends BaseState {
+    }
+    private static class ObjState extends BaseState {
     }
 
-    private final LinkedList<State> state = new LinkedList<>();
+    private final LinkedList<BaseState> state = new LinkedList<>();
 
-    private static final StringPointer emptyPtr = new StringPointer("",0);
-    private static final Whitespace newLine;
+    private static final StringPointer emptyPtr = new StringPointer("", 0);
+
+    @SuppressWarnings("rawtypes")
+    private static final Whitespace NewLine;
+
     static {
-        newLine = new Whitespace<>("\n", emptyPtr, emptyPtr);
+        NewLine = new Whitespace<>("\n", emptyPtr, emptyPtr);
     }
 
-    private void writeNewLine(){
-        output.write(newLine);
+    private void writeNewLine() {
+        output.write(NewLine);
     }
-    private void writeIndent(int level){
-        if( level>0 )output.write(indentWhitespace(level));
+
+    private void writeIndent(int level) {
+        if (level > 0) {
+            writeWhiteSpace(level * 4);
+        }
     }
-    private void writeIndent(){
+
+    private void writeIndent() {
         writeIndent(state.size());
-    }
-    private void writeWhiteSpace(int count){
-        if( count>0 )output.write(whitespace(count));
-    }
-
-    @SuppressWarnings({"rawtypes"})
-    private Whitespace indentWhitespace(int level){
-        return whitespace(level*2);
     }
 
     @SuppressWarnings({"rawtypes", "unchecked"})
-    private Whitespace whitespace(int count){
-        if( count<=0 )return new Whitespace("", emptyPtr, emptyPtr);
-        return new Whitespace(" ".repeat(count), emptyPtr, emptyPtr);
+    private void writeWhiteSpace(int count) {
+        if (count > 0) {
+            output.write(new Whitespace(" ".repeat(count), emptyPtr, emptyPtr));
+        }
     }
+
+    //////////////////////////////
+
+    private record WriteOptions() {}
+    private final Queue<Consumer<WriteOptions>> writingQueue = new LinkedList<>();
+
+    private void flush() {
+        var wo = new WriteOptions();
+        while (true) {
+            var q = writingQueue.poll();
+            if (q == null) break;
+            q.accept(wo);
+        }
+    }
+
+    private void flushIfTop() {
+        if (state.isEmpty()) {
+            flush();
+        }
+    }
+
+    //////////////////////////////
 
     @Override
     public void write(OpenParentheses<?> token) {
-        incItems();
+        addNestedItem(ItemType.JsObj);
 
-        var level = state.size();
-        state.push(new ObjState());
+        var obj = new ObjState();
+        state.push(obj);
 
-        output.write(token);
-        writeNewLine();
+        writingQueue.add(wo -> output.write(token));
+        writingQueue.add(wo -> {
+            if (obj.itemsCount > 0) {
+                writeNewLine();
+            }
+        });
     }
 
     @Override
     public void write(CloseParentheses<?> token) {
-        state.poll();
+        var st = state.poll();
 
-        writeNewLine();
-        writeIndent();
+        int level = state.size();
 
-        output.write(token);
+        writingQueue.add(wo -> {
+            if (st instanceof ObjState obj && obj.itemsCount > 0) {
+                writeNewLine();
+                writeIndent(level);
+            }
+        });
+        writingQueue.add(wo -> output.write(token));
+
+        flush();
     }
 
     @Override
     public void write(OpenSquare<?> token) {
-        incItems();
+        addNestedItem(ItemType.JsArr);
 
-        state.push(new ArrState());
+        var arr = new ArrState();
+        state.push(arr);
 
-        output.write(token);
-        writeNewLine();
+        writingQueue.add(wo -> output.write(token));
+        writingQueue.add(wo -> {
+            if (arr.itemsCount > 0) writeNewLine();
+        });
     }
 
     @Override
     public void write(CloseSquare<?> token) {
-        state.poll();
+        var st = state.poll();
 
-        writeNewLine();
-        writeIndent();
+        int level = state.size();
+        writingQueue.add(wo -> {
+            if (st instanceof ArrState arr && arr.itemsCount > 0) {
+                writeNewLine();
+                writeIndent(level);
+            }
+        });
+        writingQueue.add(wo -> output.write(token));
 
-        output.write(token);
+        flush();
     }
 
-    private void incItems(){
+    private void addNestedItem(ItemType itemType) {
         var st = state.poll();
-        if( st!=null ) {
-            state.push(st);
-            st.incItemsCount();
-            var cnt = st.getItemsCount();
-            if( st instanceof ObjState && (cnt % 2 == 1) ){
-                writeIndent(state.size());
-            }else if( st instanceof ArrState ){
-                writeIndent(state.size());
-            }
+        if (st == null) return;
+
+        state.push(st);
+        st.addItem(itemType);
+        var cnt = st.itemsCount;
+
+        int level = state.size();
+
+        if (
+            (st instanceof ObjState && (cnt % 2 == 1)) ||
+                (st instanceof ArrState)
+        ) {
+            writingQueue.add(wo -> {
+                if (st.compoundCount() > 0 || st.nestedWriteCount == 0 || st instanceof ObjState) {
+                    writeIndent(level);
+                    st.nestedWriteCount++;
+                }
+            });
         }
     }
 
     @Override
     public void write(BigIntToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.BigInt);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(LongToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Long);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(IntToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Int);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(DoubleToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Double);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(StringToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.String);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(FalseToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Bool);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(TrueToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Bool);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(NullToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Null);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(IdentifierToken<?> token) {
-        incItems();
-        output.write(token);
+        addNestedItem(ItemType.Ident);
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
     }
 
     @Override
     public void write(Colon<?> token) {
-        output.write(token);
-        writeWhiteSpace(1);
+        writingQueue.add(wo -> output.write(token));
+        writingQueue.add(wo -> writeWhiteSpace(1));
     }
 
     @Override
     public void write(Comma<?> token) {
-        output.write(token);
-        writeNewLine();
+        writingQueue.add(wo -> output.write(token));
+
+        var st = state.peek();
+        writingQueue.add(wo -> {
+            if (st != null && (
+                st.compoundCount() > 0 || st instanceof ObjState
+            )) {
+                writeNewLine();
+            }
+        });
     }
 
     @Override
-    public void write(MLComment<?> token) {output.write(token);}
+    public void write(MLComment<?> token) {
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
+    }
 
     @Override
-    public void write(SLComment<?> token) {output.write(token);}
+    public void write(SLComment<?> token) {
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
+    }
 
     @Override
-    public void write(Whitespace<?> token) {output.write(token);}
+    public void write(Whitespace<?> token) {
+        writingQueue.add(wo -> output.write(token));
+        flushIfTop();
+    }
 }
